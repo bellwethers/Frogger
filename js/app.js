e5f60c14fa4cc9c8d015de737e78df8e89bc39c4
krystin perry <perrykrys@aol.com>
2018-07-26 16:50:04 -0400

    connected player class to chara

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ class chara {
   }
 };
 
+chara.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 
 //-----ENEMY-----//
 class Enemy extends chara{
@@ -56,12 +60,12 @@ Enemy.prototype.update = function(dt) {
     //collision? If x coordinates are the same then their touching
     /* The # is the object's gap(height or width/the space the enemy takes)
     https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection*/
-    if (player.x < this.x + 60 &&
-        player.x + 60 > this.x &&
-        player.y < this.y + 60 &&
-        player.y + 60 > this.y) {
-        player.again();
-        //console.log("reset player");
+    if (_player.x < this.x + 60 &&
+        _player.x + 60 > this.x &&
+        _player.y < this.y + 60 &&
+        _player.y + 60 > this.y) {
+        _player.again();
+        console.log("reset player");
         //console.log(allEnemies.indexOf(this) , this.y+ " speed "+ this.speed);
     };
 
@@ -82,33 +86,41 @@ Enemy.prototype.update = function(dt) {
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+/*Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+};*/
 
 //-------PLAYER------//
+class player extends chara{
+  constructor (x, y, sprite){
+    super();
+    this.x = 200;
+    this.y = 400
+    this.sprite = 'images/char-princess-girl.png';
+    }
+};
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var player = function() {
+/*var player = function() {
     this.y = 400;
     this.x = 200;
     this.sprite = 'images/char-princess-girl.png';
 
-};
+};*/
 
 player.prototype.again = function() {
-    player.y = 400;
-    player.x = 200;
+    _player.y = 400;
+    _player.x = 200;
     win = false;
 };
 //update()
 player.prototype.update = function(dt) {
     //Win?
-    if (player.y <= 20 && win == false) {
+    if (_player.y <= 20 && win == false) {
         win = true;
         setTimeout(function() {
-            player.again();
+            _player.again();
             alert("You win! Want to play again?");
         }, 500);
 
@@ -118,9 +130,9 @@ player.prototype.update = function(dt) {
     }
 };
 
-player.prototype.render = function() {
+/*player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+};*/
 
 player.prototype.handleInput = function(keys) {
     if (keys === "left" && this.x > 0) {
@@ -147,7 +159,7 @@ for (var i = 0; i < enemyAmount; i++) {
 };
 // Place the player object in a variable called player
 
-var player = new player();
+var _player = new player();
 //var chara = new chara();
 
 // This listens for key presses and sends the keys to your
@@ -160,5 +172,5 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    _player.handleInput(allowedKeys[e.keyCode]);
 });
